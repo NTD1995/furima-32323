cf4c7c2118c7901e0aeb8da52e8437aed10c77e0
NTD1995 <76642241+NTD1995@users.noreply.github.com>
2021-07-09 19:56:10 +0900

    商品出品機能の修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,13 +2,12 @@ class Item < ApplicationRecord
  belongs_to :user
  has_one :purchase
  has_one_attached :image
- belongs_to :shipping_area
  extend ActiveHash::Associations::ActiveRecordExtensions
  belongs_to :category
  belongs_to :days_to_ship
  belongs_to :items_status
  belongs_to :shipping_charges
-
+ belongs_to :shipping_area
  validates :name, :description, :image, presence: true
 
  validates :category_id, :days_to_ship_id, :items_status_id, :shipping_area_id, :shipping_charges_id, numericality: { other_than: 1 } 
